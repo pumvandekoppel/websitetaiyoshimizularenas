a355b9871a4d5a2885b4d39a7854b6632150ff03
Pum van de Koppel <pumvandekoppel@gmail.com>
2024-09-17 16:53:44 +0200

    fix my external link

--- reclame.js
+++ reclame.js
@@ -4,11 +4,13 @@ function reclame() {
     if (hidden == true) {
         document.getElementsByClassName("self-promotion_shown")[0].style.display = "initial";
         document.getElementsByClassName("self-promotion_hidden")[0].style.display = "none";
+        document.getElementById("website-pum").style.display = "initial"
         hidden = false
     }
     else {
         document.getElementsByClassName("self-promotion_shown")[0].style.display = "none";
         document.getElementsByClassName("self-promotion_hidden")[0].style.display= "initial";
+        document.getElementById("website-pum").style.display = "none"
         hidden = true
     }
 }
