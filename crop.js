405987a5295106e89dd24f9f4f58a2718ea4e9c3
pvdk <pumvandekoppel@gmail.com>
2025-07-16 14:49:56 +0200

    nieuwe lading

--- crop.js
+++ crop.js
@@ -19,6 +19,414 @@ $(" .modal").on("click", function (ev) {
   return false;
 }); */
 
+function crop2025_0() {
+  $(".modal").fadeIn(0);
+
+  var currentImage = '<img class="painting-crop__picture" src="';
+  currentImage += "assets/schilderij/2025/Crop2025_0.JPG";
+  currentImage += '" alt="..."/>';
+  //   $(this).find('img').prop('src', this.href);
+  //   var currentImage = $(entry.content).find('figure a').attr('href');
+
+  $(".modal-content").html(currentImage);
+}
+
+function crop2025_1() {
+  $(".modal").fadeIn(0);
+
+  var currentImage = '<img class="painting-crop__picture" src="';
+  currentImage += "assets/schilderij/2025/Crop2025_1.JPG";
+  currentImage += '" alt="..."/>';
+  //   $(this).find('img').prop('src', this.href);
+  //   var currentImage = $(entry.content).find('figure a').attr('href');
+
+  $(".modal-content").html(currentImage);
+}
+
+function crop2025_2() {
+  $(".modal").fadeIn(0);
+
+  var currentImage = '<img class="painting-crop__picture" src="';
+  currentImage += "assets/schilderij/2025/Crop2025_2.JPG";
+  currentImage += '" alt="..."/>';
+  //   $(this).find('img').prop('src', this.href);
+  //   var currentImage = $(entry.content).find('figure a').attr('href');
+
+  $(".modal-content").html(currentImage);
+}
+
+function crop2025_3() {
+  $(".modal").fadeIn(0);
+
+  var currentImage = '<img class="painting-crop__picture" src="';
+  currentImage += "assets/schilderij/2025/Crop2025_3.JPG";
+  currentImage += '" alt="..."/>';
+  //   $(this).find('img').prop('src', this.href);
+  //   var currentImage = $(entry.content).find('figure a').attr('href');
+
+  $(".modal-content").html(currentImage);
+}
+
+function crop2025_4() {
+  $(".modal").fadeIn(0);
+
+  var currentImage = '<img class="painting-crop__picture" src="';
+  currentImage += "assets/schilderij/2025/Crop2025_4.JPG";
+  currentImage += '" alt="..."/>';
+  //   $(this).find('img').prop('src', this.href);
+  //   var currentImage = $(entry.content).find('figure a').attr('href');
+
+  $(".modal-content").html(currentImage);
+}
+
+function crop2025_5() {
+  $(".modal").fadeIn(0);
+
+  var currentImage = '<img class="painting-crop__picture" src="';
+  currentImage += "assets/schilderij/2025/Crop2025_5.JPG";
+  currentImage += '" alt="..."/>';
+  //   $(this).find('img').prop('src', this.href);
+  //   var currentImage = $(entry.content).find('figure a').attr('href');
+
+  $(".modal-content").html(currentImage);
+}
+
+function crop2025_6() {
+  $(".modal").fadeIn(0);
+
+  var currentImage = '<img class="painting-crop__picture" src="';
+  currentImage += "assets/schilderij/2025/Crop2025_6.JPG";
+  currentImage += '" alt="..."/>';
+  //   $(this).find('img').prop('src', this.href);
+  //   var currentImage = $(entry.content).find('figure a').attr('href');
+
+  $(".modal-content").html(currentImage);
+}
+
+function crop2025_7() {
+  $(".modal").fadeIn(0);
+
+  var currentImage = '<img class="painting-crop__picture" src="';
+  currentImage += "assets/schilderij/2025/Crop2025_7.JPG";
+  currentImage += '" alt="..."/>';
+  //   $(this).find('img').prop('src', this.href);
+  //   var currentImage = $(entry.content).find('figure a').attr('href');
+
+  $(".modal-content").html(currentImage);
+}
+
+function crop2025_8() {
+  $(".modal").fadeIn(0);
+
+  var currentImage = '<img class="painting-crop__picture" src="';
+  currentImage += "assets/schilderij/2025/Crop2025_8.JPG";
+  currentImage += '" alt="..."/>';
+  //   $(this).find('img').prop('src', this.href);
+  //   var currentImage = $(entry.content).find('figure a').attr('href');
+
+  $(".modal-content").html(currentImage);
+}
+
+function crop2025_9() {
+  $(".modal").fadeIn(0);
+
+  var currentImage = '<img class="painting-crop__picture" src="';
+  currentImage += "assets/schilderij/2025/Crop2025_9.JPG";
+  currentImage += '" alt="..."/>';
+  //   $(this).find('img').prop('src', this.href);
+  //   var currentImage = $(entry.content).find('figure a').attr('href');
+
+  $(".modal-content").html(currentImage);
+}
+
+function crop2025_10() {
+  $(".modal").fadeIn(0);
+
+  var currentImage = '<img class="painting-crop__picture" src="';
+  currentImage += "assets/schilderij/2025/Crop2025_10.JPG";
+  currentImage += '" alt="..."/>';
+  //   $(this).find('img').prop('src', this.href);
+  //   var currentImage = $(entry.content).find('figure a').attr('href');
+
+  $(".modal-content").html(currentImage);
+}
+
+function crop2025_11() {
+  $(".modal").fadeIn(0);
+
+  var currentImage = '<img class="painting-crop__picture" src="';
+  currentImage += "assets/schilderij/2025/Crop2025_11.JPG";
+  currentImage += '" alt="..."/>';
+  //   $(this).find('img').prop('src', this.href);
+  //   var currentImage = $(entry.content).find('figure a').attr('href');
+
+  $(".modal-content").html(currentImage);
+}
+
+function crop2025_12() {
+  $(".modal").fadeIn(0);
+
+  var currentImage = '<img class="painting-crop__picture" src="';
+  currentImage += "assets/schilderij/2025/Crop2025_12.JPG";
+  currentImage += '" alt="..."/>';
+  //   $(this).find('img').prop('src', this.href);
+  //   var currentImage = $(entry.content).find('figure a').attr('href');
+
+  $(".modal-content").html(currentImage);
+}
+
+function crop2025_13() {
+  $(".modal").fadeIn(0);
+
+  var currentImage = '<img class="painting-crop__picture" src="';
+  currentImage += "assets/schilderij/2025/Crop2025_13.JPG";
+  currentImage += '" alt="..."/>';
+  //   $(this).find('img').prop('src', this.href);
+  //   var currentImage = $(entry.content).find('figure a').attr('href');
+
+  $(".modal-content").html(currentImage);
+}
+
+function crop2025_14() {
+  $(".modal").fadeIn(0);
+
+  var currentImage = '<img class="painting-crop__picture" src="';
+  currentImage += "assets/schilderij/2025/Crop2025_14.JPG";
+  currentImage += '" alt="..."/>';
+  //   $(this).find('img').prop('src', this.href);
+  //   var currentImage = $(entry.content).find('figure a').attr('href');
+
+  $(".modal-content").html(currentImage);
+}
+
+function crop2025_15() {
+  $(".modal").fadeIn(0);
+
+  var currentImage = '<img class="painting-crop__picture" src="';
+  currentImage += "assets/schilderij/2025/Crop2025_15.JPG";
+  currentImage += '" alt="..."/>';
+  //   $(this).find('img').prop('src', this.href);
+  //   var currentImage = $(entry.content).find('figure a').attr('href');
+
+  $(".modal-content").html(currentImage);
+}
+
+function crop2025_16() {
+  $(".modal").fadeIn(0);
+
+  var currentImage = '<img class="painting-crop__picture" src="';
+  currentImage += "assets/schilderij/2025/Crop2025_16.JPG";
+  currentImage += '" alt="..."/>';
+  //   $(this).find('img').prop('src', this.href);
+  //   var currentImage = $(entry.content).find('figure a').attr('href');
+
+  $(".modal-content").html(currentImage);
+}
+
+function crop2025_17() {
+  $(".modal").fadeIn(0);
+
+  var currentImage = '<img class="painting-crop__picture" src="';
+  currentImage += "assets/schilderij/2025/Crop2025_17.JPG";
+  currentImage += '" alt="..."/>';
+  //   $(this).find('img').prop('src', this.href);
+  //   var currentImage = $(entry.content).find('figure a').attr('href');
+
+  $(".modal-content").html(currentImage);
+}
+
+function crop2025_18() {
+  $(".modal").fadeIn(0);
+
+  var currentImage = '<img class="painting-crop__picture" src="';
+  currentImage += "assets/schilderij/2025/Crop2025_18.JPG";
+  currentImage += '" alt="..."/>';
+  //   $(this).find('img').prop('src', this.href);
+  //   var currentImage = $(entry.content).find('figure a').attr('href');
+
+  $(".modal-content").html(currentImage);
+}
+
+function crop2025_19() {
+  $(".modal").fadeIn(0);
+
+  var currentImage = '<img class="painting-crop__picture" src="';
+  currentImage += "assets/schilderij/2025/Crop2025_19.JPG";
+  currentImage += '" alt="..."/>';
+  //   $(this).find('img').prop('src', this.href);
+  //   var currentImage = $(entry.content).find('figure a').attr('href');
+
+  $(".modal-content").html(currentImage);
+}
+
+function crop2025_20() {
+  $(".modal").fadeIn(0);
+
+  var currentImage = '<img class="painting-crop__picture" src="';
+  currentImage += "assets/schilderij/2025/Crop2025_20.JPG";
+  currentImage += '" alt="..."/>';
+  //   $(this).find('img').prop('src', this.href);
+  //   var currentImage = $(entry.content).find('figure a').attr('href');
+
+  $(".modal-content").html(currentImage);
+}
+
+function crop2025_21() {
+  $(".modal").fadeIn(0);
+
+  var currentImage = '<img class="painting-crop__picture" src="';
+  currentImage += "assets/schilderij/2025/Crop2025_21.JPG";
+  currentImage += '" alt="..."/>';
+  //   $(this).find('img').prop('src', this.href);
+  //   var currentImage = $(entry.content).find('figure a').attr('href');
+
+  $(".modal-content").html(currentImage);
+}
+
+function crop2024_1() {
+  $(".modal").fadeIn(0);
+
+  var currentImage = '<img class="painting-crop__picture" src="';
+  currentImage += "assets/schilderij/2024/Crop1.JPG";
+  currentImage += '" alt="..."/>';
+  //   $(this).find('img').prop('src', this.href);
+  //   var currentImage = $(entry.content).find('figure a').attr('href');
+
+  $(".modal-content").html(currentImage);
+}
+
+function crop2024_2() {
+  $(".modal").fadeIn(0);
+
+  var currentImage = '<img class="painting-crop__picture" src="';
+  currentImage += "assets/schilderij/2024/Crop2.JPG";
+  currentImage += '" alt="..."/>';
+  //   $(this).find('img').prop('src', this.href);
+  //   var currentImage = $(entry.content).find('figure a').attr('href');
+
+  $(".modal-content").html(currentImage);
+}
+
+function crop2024_3() {
+  $(".modal").fadeIn(0);
+
+  var currentImage = '<img class="painting-crop__picture" src="';
+  currentImage += "assets/schilderij/2024/Crop3.JPG";
+  currentImage += '" alt="..."/>';
+  //   $(this).find('img').prop('src', this.href);
+  //   var currentImage = $(entry.content).find('figure a').attr('href');
+
+  $(".modal-content").html(currentImage);
+}
+
+function crop2024_4() {
+  $(".modal").fadeIn(0);
+
+  var currentImage = '<img class="painting-crop__picture" src="';
+  currentImage += "assets/schilderij/2024/Crop4.JPG";
+  currentImage += '" alt="..."/>';
+  //   $(this).find('img').prop('src', this.href);
+  //   var currentImage = $(entry.content).find('figure a').attr('href');
+
+  $(".modal-content").html(currentImage);
+}
+
+function crop2024_5() {
+  $(".modal").fadeIn(0);
+
+  var currentImage = '<img class="painting-crop__picture" src="';
+  currentImage += "assets/schilderij/2024/Crop5.JPG";
+  currentImage += '" alt="..."/>';
+  //   $(this).find('img').prop('src', this.href);
+  //   var currentImage = $(entry.content).find('figure a').attr('href');
+
+  $(".modal-content").html(currentImage);
+}
+
+function crop2024_6() {
+  $(".modal").fadeIn(0);
+
+  var currentImage = '<img class="painting-crop__picture" src="';
+  currentImage += "assets/schilderij/2024/Crop6.JPG";
+  currentImage += '" alt="..."/>';
+  //   $(this).find('img').prop('src', this.href);
+  //   var currentImage = $(entry.content).find('figure a').attr('href');
+
+  $(".modal-content").html(currentImage);
+}
+
+function crop2024_7() {
+  $(".modal").fadeIn(0);
+
+  var currentImage = '<img class="painting-crop__picture" src="';
+  currentImage += "assets/schilderij/2024/Crop7.JPG";
+  currentImage += '" alt="..."/>';
+  //   $(this).find('img').prop('src', this.href);
+  //   var currentImage = $(entry.content).find('figure a').attr('href');
+
+  $(".modal-content").html(currentImage);
+}
+
+function crop2024_8() {
+  $(".modal").fadeIn(0);
+
+  var currentImage = '<img class="painting-crop__picture" src="';
+  currentImage += "assets/schilderij/2024/Crop8.JPG";
+  currentImage += '" alt="..."/>';
+  //   $(this).find('img').prop('src', this.href);
+  //   var currentImage = $(entry.content).find('figure a').attr('href');
+
+  $(".modal-content").html(currentImage);
+}
+
+function crop2024_9() {
+  $(".modal").fadeIn(0);
+
+  var currentImage = '<img class="painting-crop__picture" src="';
+  currentImage += "assets/schilderij/2024/Crop9.JPG";
+  currentImage += '" alt="..."/>';
+  //   $(this).find('img').prop('src', this.href);
+  //   var currentImage = $(entry.content).find('figure a').attr('href');
+
+  $(".modal-content").html(currentImage);
+}
+
+function crop2024_10() {
+  $(".modal").fadeIn(0);
+
+  var currentImage = '<img class="painting-crop__picture" src="';
+  currentImage += "assets/schilderij/2024/Crop10.JPG";
+  currentImage += '" alt="..."/>';
+  //   $(this).find('img').prop('src', this.href);
+  //   var currentImage = $(entry.content).find('figure a').attr('href');
+
+  $(".modal-content").html(currentImage);
+}
+
+function crop2024_11() {
+  $(".modal").fadeIn(0);
+
+  var currentImage = '<img class="painting-crop__picture" src="';
+  currentImage += "assets/schilderij/2024/Crop11.JPG";
+  currentImage += '" alt="..."/>';
+  //   $(this).find('img').prop('src', this.href);
+  //   var currentImage = $(entry.content).find('figure a').attr('href');
+
+  $(".modal-content").html(currentImage);
+}
+
+function crop2024_12() {
+  $(".modal").fadeIn(0);
+
+  var currentImage = '<img class="painting-crop__picture" src="';
+  currentImage += "assets/schilderij/2024/Crop12.JPG";
+  currentImage += '" alt="..."/>';
+  //   $(this).find('img').prop('src', this.href);
+  //   var currentImage = $(entry.content).find('figure a').attr('href');
+
+  $(".modal-content").html(currentImage);
+}
+
 function crop26() {
   $(".modal").fadeIn(0);
 
